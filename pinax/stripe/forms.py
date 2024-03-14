73f1ee1b1167b77ec497250472f8117d62d87616
Gordon Vu <gordon.vu@gmail.com>
2024-03-14 16:07:55 +1100

    django.utils.translation.ugettext_lazy() is deprecated in favor of django.utils.translation.gettext_lazy()

--- pinax/stripe/forms.py
+++ pinax/stripe/forms.py
@@ -2,7 +2,7 @@ import datetime
 import time
 
 from django import forms
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 
 import stripe
 from ipware import get_client_ip
